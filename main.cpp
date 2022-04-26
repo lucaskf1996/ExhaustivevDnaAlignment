dce33dbea096f708d563d92aa6da3573d4d40bd4
lucaskf1996 <lucaskf1996@gmail.com>
2022-04-26 18:52:10 +0000

    removidos elementos repetidos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 #include<algorithm>
 #include<fstream>  
+#include <vector>
+
+int strCompare(std::string subA, std::string subB, int lenSubstr){
+    int totalScore = 0;
+    for(int i = 0; i<lenSubstr; i++){
+            if(subA[i] == subB[i]){
+                totalScore+=2;
+            }
+            else{
+                totalScore-=1;
+            }
+        }
+    return totalScore;
+}
 
 int main(){
     int n, m;
@@ -9,6 +23,8 @@ int main(){
     std::cin >> m;
     std::cin >> a;
     std::cin >> b;
+    std::vector <std::string> ListaA{};
+    std::vector <std::string> ListaB{};
     int lenSubstr, indexA, indexB, totalScore;
     int totalScoreGlobal = 0;
     std::string bestA, bestB;
@@ -18,34 +34,39 @@ int main(){
     else{
         lenSubstr = n;
     }
+    int quantidade = 0;
     while(lenSubstr>=2){
         indexA = 0;
         while(indexA<n-lenSubstr){
             std::string subA = a.substr(indexA, lenSubstr);
-            indexB = 0;
-            while(indexB<m-lenSubstr){
-                std::string subB = b.substr(indexB, lenSubstr);
-                totalScore = 0;
-                for(int i = 0; i<lenSubstr; i++){
-                    if(subA[i] == subB[i]){
-                        totalScore+=2;
+            auto foundA = std::find(ListaA.begin(), ListaA.end(), subA);
+            if(foundA == ListaA.end()){
+                ListaA.push_back(subA);
+                indexB = 0;
+                while(indexB<m-lenSubstr){
+                    std::string subB = b.substr(indexB, lenSubstr);
+                    auto foundB = std::find(ListaB.begin(), ListaB.end(), subB);
+                    if(foundB == ListaB.end()){
+                        ListaB.push_back(subB);
+                        totalScore = strCompare(subA, subB, lenSubstr);
+                        quantidade++;
+                        if(totalScoreGlobal<totalScore){
+                            totalScoreGlobal = totalScore;
+                            bestA = subA;
+                            bestB = subB;
+                        }
                     }
-                    else{
-                        totalScore-=1;
-                    }
+                    indexB++;
+                    // std::cout << "subA: " << subA << std::endl << "subB: " << subB << std::endl;
                 }
-                if(totalScoreGlobal<totalScore){
-                    totalScoreGlobal = totalScore;
-                    bestA = subA;
-                    bestB = subB;
-                }
-                indexB++;
-                // std::cout << "subA: " << subA << std::endl << "subB: " << subB << std::endl;
+                ListaB.clear();
             }
             indexA++;
         }
         lenSubstr--;
+        ListaA.clear();
     }
+    std::cout << "combinacoes: " << quantidade << std::endl;
     std::ofstream outfile ("output.txt");
     outfile << totalScoreGlobal << std::endl << "bestA: " << bestA << std::endl << "bestB: " << bestB << std::endl;
     outfile.close();
